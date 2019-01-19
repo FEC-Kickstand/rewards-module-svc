9516560b5952acec8f40549f2c9d5df549f204d1
David Lee <edenlhn@gmail.com>
2019-01-18 17:17:52 -0800

    Deleted unnecessary var from server.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
 const db = require('../database/index.js');
 
 const app = express();
